003b1cc36a0d359abcdb267198a03ee33c1c68a2
George Dobrin <georgedobrin11@gmail.com>
2020-02-09 18:24:09 +0100

    api: initial setup+routing

---
+++ routes/routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// const User = require('../models/user');
+app.use(cors());
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({'response': '/ works'});
+})
+
+router.get('/public', (req, res, next) => {
+    res.status(200).json({'response': '/public works'});
+})
+
+router.get('/username', (req, res, next) => {
+    res.status(200).json({'response': '/username works'});
+})
+
+router.get('/username/follow', (req, res, next) => {
+    res.status(200).json({'response': '/username/follow works'});
+})
+
+router.get('/username/unfollow', (req, res, next) => {
+    res.status(200).json({'response': '/username/unfollow works'});
+})
+
+router.post('/username/add_message', (req, res, next) => {
+    res.status(200).json({'response': '/username/add_message works'});
+})
+
+router.get('/login', (req, res, next) => {
+    res.status(200).json({'response': '/login works'});
+})
+
+router.post('/login', (req, res, next) => {
+    res.status(200).json({'response': '/login works'});
+})
+
+router.get('/register', (req, res, next) => {
+    res.status(200).json({'response': '/register works'});
+})
+
+router.post('/register', (req, res, next) => {
+    res.status(200).json({'response': '/register works'});
+})
+
+router.get('/logout', (req, res, next) => {
+    res.status(200).json({'response': '/logout works'});
+})
+
+module.exports = router;
