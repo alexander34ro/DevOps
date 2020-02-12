942b26fe5b24bfe89f6f4fe6a492b0effdf32c5d
Your Name <you@example.com>
2020-02-12 15:56:59 +0100

    add SignIn page

--- refactored_remote_code/client/src/App.js
+++ refactored_remote_code/client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Nav from "./components/Nav"
+import Flashes from "./components/Flashes"
+import Login from "./components/Login"
 
 function App() {
+  // TODO: fetches flash message
   const dummy_flashes = ["this is a test flash msg", "this is another test flash msg"]
   return (
     <div className="App">
       <Nav name="joe" />
     <div className="page">
         <h1>MiniTwit</h1>
-      <Flashes flashes_msgs={dummy_flashes} />
+      <Flashes msgs={dummy_flashes} />
         <div className="body">
           {/* {'{'}% block body %{'}'}{'{'}% endblock %{'}'} */}
+          <Login/>
         </div>
         <div className="footer">
           MiniTwit — A Flask Application
@@ -20,15 +24,5 @@ function App() {
   );
 }
 
-function Flashes(props){
-  const msgs = props.flashes_msgs
-  if (msgs) {
-    const flashes = msgs.map((msg) =>
-                             <li> {msg} </li>
-                            )
-   return <ul className="flashes">{flashes}</ul>
-  }
-
-}
 
 export default App;
--- refactored_remote_code/client/src/components/Login.js
+++ refactored_remote_code/client/src/components/Login.js
@@ -1,16 +1,45 @@
-{% extends "layout.html" %}
-{% block title %}Sign In{% endblock %}
-{% block body %}
-  <h2>Sign In</h2>
-  {% if error %}<div class=error><strong>Error:</strong> {{ error }}</div>{% endif %}
-  <form action="" method=post>
-    <dl>
-      <dt>Username:
-      <dd><input type=text name=username size=30 value="{{ request.form.username }}">
-      <dt>Password:
-      <dd><input type=password name=password size=30>
-    </dl>
-    <div class=actions><input type=submit value="Sign In"></div>
-  </form>
-{% endblock %}
+import React, { Component } from 'react'
 
+class Login extends Component {
+  constructor(props) {
+    super(props) ;
+    let name = props.name ? props.name: "nobody"
+    this.state = {
+      isLogged: true,
+      username : name,
+    }
+  }
+  componentDidMount(){
+    document.title = "Sign in"
+  }
+  render() {
+    const isLogged = this.state.isLogged;
+    const username = this.state.username;
+    // TODO: fetch error msgs
+    const error = "oupsii";
+    let errorEl = null;
+    if (error)
+      errorEl = <div className="error"><strong>Error:</strong> {error}</div>
+    return (
+      <div>
+      <h2>Sign In </h2>
+      {errorEl}
+      <div>
+        <form action method="post">
+          <dl>
+            <dt>Username:
+            </dt><dd><input type="text" name="username" size={30} defaultValue="{{ request.form.username }}" />
+            </dd><dt>Password:
+            </dt><dd><input type="password" name="password" size={30} />
+            </dd></dl>
+          <div className="actions"><input type="submit" defaultValue="Sign In" /></div>
+        </form>
+      </div>
+      </div>
+    );
+
+
+  }
+}
+
+export default Login;
