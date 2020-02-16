7ea03138172fc0113811a0d359c503047acf6d44
Jianu_Petrut <c.petrut97@gmail.com>
2020-02-16 13:59:11 +0100

    add register/login/ add message tests

--- test.js
+++ test.js
@@ -33,6 +33,7 @@ test.after.always(async () => {
 	mongoose.disconnect()
 	mongod.stop()
 })
+
 test.serial('register', async t => {
   let rv=null;
  // const {app} = t.context;
@@ -63,14 +64,61 @@ test.serial('register', async t => {
   const rem = await User.remove({"username":mockUser.username});
  
 });
-test("login_logout", t => {
+
+
+
+
+test.serial("login", async t => {
   let rv = null;
-  t.pass();
+  const reg = await request(app)
+		.post('/register')
+		.send({
+			"email": "testy@testoo.com",
+      "username":"testyTest",
+      "password": "testyPass"
+      
+		}).set('Accept', 'application/json');
+
+	t.is(reg.status, 200);
+  const log = await request(app).post('/login').send({
+    "password":"testyPass",
+    "username":"testyTest"
+  }).set('Accept', 'application/json');
+
+  t.is(log.status,200);
+
+  const rem = await User.remove({"username":"testyTest"});
 });
 
-test("message_recording", t => {
+test("message_recording", async t => {
   let rv = null;
-  t.pass();
+
+  const reg = await request(app)
+		.post('/register')
+		.send({
+			"email": "testy@testoo.com",
+      "username":"testyTest",
+      "password": "testyPass"
+      
+		}).set('Accept', 'application/json');
+
+	t.is(reg.status, 200);
+  const log = await request(app).post('/login').send({
+    "password":"testyPass",
+    "username":"testyTest"
+  }).set('Accept', 'application/json');
+
+  t.is(log.status,200);
+  const msg = await request(app).post('/add_message').send({
+    "text":"someTestyText",
+    "flagged":false
+  }).set('Accept', 'application/json');
+
+  t.is(msg.status,200);
+  let user = await User.findOne({"username":"testyTest"});
+  const rem = await Message.remove({"author_id":user._id});
+  const remUser = await User.remove({"username":"testyTest"});
+  
 });
 
 test("timelines", t => {
