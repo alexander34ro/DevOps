efd1bd4fc19c4e27535e30758d1efecbe7c81af7
joe <you@example.com>
2020-02-16 16:32:29 +0100

    add basic dummy front end. One big commit with all the react files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
 import React from 'react';
-import Nav from "./components/Nav"
-import Flashes from "./components/Flashes"
-import Login from "./components/Login"
-import Register from "./components/Register"
-import Timeline from "./components/Timeline"
+import logo from './logo.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import TwitMenu from './components/TwitMenu'
+import TwitFeed from './components/TwitFeed'
+import SignIn from './components/SignIn.js'
+import SignUp from './components/SignUp.js'
 
-function App() {
-  // TODO: fetches flash message
-  const dummy_flashes = ["this is a test flash msg", "this is another test flash msg"]
+import {
+  Container,
+  Message,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
   return (
-    <div className="App">
-    <div className="page">
-        <h1>MiniTwit</h1>
-      <Nav name="joe" />
-      <Flashes msgs={dummy_flashes} />
-        <div className="body">
-          {/* {'{'}% block body %{'}'}{'{'}% endblock %{'}'} */}
-          {/* <Login/> */}
-          {/* <Register/> */}
-          <Timeline/>
-        </div>
-        <div className="footer">
-          MiniTwit — A Flask Application
-        </div>
-      </div>
-    </div>
+    <Router>
+    <TwitMenu/>
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <PublicFeed />
+          </Route>
+          <Route path="/public">
+            <PublicFeed />
+          </Route>
+          <Route path="/signin">
+            <SignIn/>
+          </Route>
+          <Route path="/signup">
+            <SignUp/>
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
+// You can think of these components as "pages"
+// in your app.
 
-export default App;
+
+
+
+const PublicFeed = () => (
+  <div>
+
+    <Container text style={{ marginTop: '7em' }}>
+    <Message success>
+      <Message.Header>Login Successfull</Message.Header>
+      <p>Did you know it's been a while?</p>
+    </Message>
+    <Message warning>
+      <Message.Header>Login Failled</Message.Header>
+      <p>Did you know it's been a while?</p>
+    </Message>
+      <Header as='h1'>Public Feed</Header>
+      <TwitFeed/>
+    </Container>
+
+  </div>
+)
---
+++ src/components/TwitMenu.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import { Link } from 'react-router-dom'
+import {
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Image,
+  List,
+  Menu,
+  Segment,
+} from 'semantic-ui-react'
+
+export default function TwitMenu() {
+    return (
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item  as={Link} to="/"  header>
+          MiniTwit
+        </Menu.Item>
+        <Menu.Item as={Link} to="/public" >Public Timeline</Menu.Item>
+        <Menu.Item  as={Link} to="/signup" >Sign Up</Menu.Item>
+        <Menu.Item  as={Link} to="/signin" >Sign In</Menu.Item>
+
+      </Container>
+    </Menu>
+
+    )
+}
