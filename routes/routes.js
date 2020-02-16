d0a588adda20fe03ca6a5af59e59c91cbc703107
geoox <georgedobrin11@gmail.com>
2020-02-16 16:43:00 +0100

    almost finished. need to solve an async issue

--- routes/routes.js
+++ routes/routes.js
@@ -9,20 +9,22 @@ const Message = require('../models/message');
 
 let LATEST = 0;
 
-function get_user_id(username){
-    User.findOne({
-        'username': username
-    })
-    .then(user => {
-        return user._id
-    })
-    .catch(err => {
-        console.log('err', err);
-        return 0;
-    })
+function get_user_id(username) {
+    return new Promise((resolve, reject) => {
+        User.findOne({
+            'username': username
+        })
+            .then(user => {
+                resolve(user._id);
+            })
+            .catch(err => {
+                console.log('err', err);
+                reject();
+            })
+    });
 }
 
-function update_latest(value){
+function update_latest(value) {
     LATEST = value != -1 ? value : LATEST;
 }
 
@@ -35,17 +37,17 @@ router.get("/latest", (req, res, next) => {
 router.post("/register", (req, res, next) => {
     update_latest(req.body.latest);
     let err = "";
-    if (!req.body.username){
+    if (!req.body.username) {
         err = "You have to enter a username";
-    } else if(!req.body.email || (!req.body.email.includes("@") && !req.body.email.includes("."))){
+    } else if (!req.body.email || (!req.body.email.includes("@") && !req.body.email.includes("."))) {
         err = "You have to enter a valid email address";
-    } else if(!req.body.password){
+    } else if (!req.body.password) {
         err = "You have to enter a password";
-    } else if(get_user_id(req.body.username) != undefined){
+    } else if (get_user_id(req.body.username) != undefined) {
         err = "Username already taken";
     } else {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
-            if(err){
+            if (err) {
                 return res.status(500).json({
                     error: err
                 })
@@ -57,56 +59,174 @@ router.post("/register", (req, res, next) => {
                     pw_hash: hash
                 })
                 newUser.save()
-                .then(result => {
-                    console.log("added new user", result);
-                    res.status(204);
-                })
-                .catch(err => res.status(500).json({
-                    error: err
-                }));
+                    .then(result => {
+                        console.log("added new user", result);
+                        res.status(204);
+                    })
+                    .catch(err => res.status(500).json({
+                        error: err
+                    }));
             }
         });
     }
-    if(err.length != 0){
+    if (err.length != 0) {
         res.status(400).json({
             "error_msg": err
         })
     }
 })
 
+
+// TODO: solve async
 router.get("/msgs", (req, res, next) => {
     update_latest(req.body.latest);
     const number_messages = req.body.no;
     Message.find()
-    .limit(number_messages)
-    .then(messagesResult => {
-        let filtered_msgs = [];
+        .limit(number_messages)
+        .sort({
+            "pub_date": -1
+        })
+        .then(messagesResult => {
+            let filtered_msgs = [];
 
-        const promises = messagesResult.map(async message => {
-            const user = await User.findById(message.author_id);
-            return user;
+            for (const message of messagesResult) {
+                User.findById(message.author_id)
+                    .then(user => {
+                        filtered_msg = {};
+                        filtered_msg['content'] = message.text;
+                        filtered_msg['pub_date'] = message.pub_date;
+                        filtered_msg['user'] = user.username;
+                        filtered_msgs.push(filtered_msg);
+                        console.log(filtered_msgs);
+                    })
+                    .catch(err => console.log(err));
+            }
+            console.log(filtered_msgs);
+            res.status(200).json({ filtered_msgs });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err })
         });
+})
 
-        const users = await Promise.all(promises);
+router.get("/msgs/:username", async (req, res, next) => {
+    update_latest(req.body.latest);
+    const number_messages = req.body.no;
 
-        for(const message of messagesResult){
-            User.findById(message.author_id)
-            .then(user => {
+    const user_id = await get_user_id(req.params.username);
+
+    if (!user_id) return res.status(404).json({ "err": "User not found" });
+
+    Message.find({
+        'author_id': user_id
+    })
+        .limit(number_messages)
+        .sort({
+            "pub_date": -1
+        })
+        .then(userMessages => {
+            console.log('userMessages', userMessages)
+            var filtered_msgs = [];
+
+            userMessages.forEach(message => {
+                console.log('message', message)
                 filtered_msg = {};
                 filtered_msg['content'] = message.text;
                 filtered_msg['pub_date'] = message.pub_date;
-                filtered_msg['user'] = user.username;
+                filtered_msg['user'] = req.params.username;
                 filtered_msgs.push(filtered_msg);
-                return filtered_msgs;
             })
-            .catch(err => console.log(err));
-        }
-        console.log(filtered_msgs);
-        res.status(200).json({ filtered_msgs });
-    })
-    .catch(err => {
-        res.status(500).json({ error: err })
+            res.status(200).json({ filtered_msgs });
+
+        })
+        .catch(err => {
+            res.status(500).json({ error: err })
+        })
+})
+
+router.post("/msgs/:username", async (req, res, next) => {
+    update_latest(req.body.latest);
+
+    const user_id = await get_user_id(req.params.username);
+    if (!user_id) return res.status(404).json({ "err": "User not found" });
+
+    const newMessage = new Message({
+        message_id: new mongoose.Types.ObjectId(),
+        author_id: user_id,
+        text: req.body.text,
+        pub_date: new Date(),
+        flagged: req.body.flagged
     });
+    newMessage
+        .save()
+        .then(result => {
+            res.status(204).json(result);
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+router.get("/fllws/:username", async (req, res, next) => {
+    update_latest(req.body.latest);
+    const user_id = await get_user_id(req.params.username);
+    if (!user_id) return res.status(404).json({ "err": "User not found" });
+
+    Follower.find({
+        "who_id": user_id
+    })
+        .then(followers => {
+            const follows = [];
+            followers.map(follower => {
+                follows.push(follower.whom_id);
+            })
+            console.log('follows', follows);
+            res.status(200).json({
+                "follows": follows
+            })
+        })
+        .catch(err => res.status(500).json(err));
+})
+
+router.post("/fllws/:username", async (req, res, next) => {
+    update_latest(req.body.latest);
+
+    const user_id = await get_user_id(req.params.username);
+    if (!user_id) return res.status(404).json({ "err": "User not found" });
+
+    if (req.body.keys == "follow") {
+        to_follow_user_id = req.body.follow;
+        if (!to_follow_user_id) {
+            res.status(404).json({
+                "message": "no username to follow found"
+            })
+        } else {
+            const newFollower = new Follower({
+                who_id: user_id,
+                whom_id: to_follow_user_id
+            });
+            newFollower.save()
+                .then(result => {
+                    res.status(204).json(result);
+                })
+                .catch(err => console.log(err));
+        }
+
+    } else if (req.body.keys == "unfollow") {
+        to_unfollow_user_id = req.body.unfollow;
+        if (!to_unfollow_user_id) {
+            res.status(404).json({
+                "message": "no user to unfollow found"
+            })
+        }
+
+        Follower.findOneAndDelete({
+            "who_id": user_id,
+            "whom_id": to_unfollow_user_id
+        }).then(result => {
+            res.status(204).json(result);
+        })
+
+    }
+
 })
 
 module.exports = router;
