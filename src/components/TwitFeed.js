3ca75601303bebe5f2234b6135fd667fae47b5f7
joe <you@example.com>
2020-02-19 20:22:49 +0100

    refactored TwitFeed to react class

--- src/components/TwitFeed.js
+++ src/components/TwitFeed.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { Feed, Icon } from 'semantic-ui-react'
 import Message from './Message'
 
-const TwitFeed = () => (
-  <Feed size="large">
-    <Message/>
-    <Message/>
-    <Message/>
-    <Message/>
-    <Message/>
-  </Feed>
-)
+// const TwitFeed = () => (
+class TwitFeed extends React.Component {
+  render() {
+    return (
+      <Feed size="large">
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+      </Feed>
+
+    )
+
+  }
+}
 
 export default TwitFeed
