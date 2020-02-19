cd14846ba161abc2c896c81feae684cc7c4c98a2
joe <you@example.com>
2020-02-19 20:40:44 +0100

    add fetching the api

--- src/components/TwitFeed.js
+++ src/components/TwitFeed.js
@@ -4,9 +4,53 @@ import Message from './Message'
 
 // const TwitFeed = () => (
 class TwitFeed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+    componentDidMount() {
+    fetch("https://minitwit-api.herokuapp.com/public")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.result
+          });
+          console.log("success fetching api")
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+            console.log("fail fetching api")
+        }
+      )
+  }
   render() {
+    const { error, isLoaded, items } = this.state;
+    console.log("items:")
+    console.log(items)
+    console.log("--")
     return (
       <Feed size="large">
+        <ul>
+          {items.map(item => (
+            <li key={item._id}>
+              {item.author_id}
+              {item.pub_date}
+              {item.text}
+            </li>
+          ))}
+        </ul>
         <Message/>
         <Message/>
         <Message/>
